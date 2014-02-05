f2667954bd958849e761782f2599864365b3eeb6
Jiří Suchomel <jsuchome@suse.cz>
2014-02-05 09:45:28 +0100

    different check for running chef-client

---
+++ library/commandline/src/clients/chef_warning.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+
+# ***************************************************************************
+#
+# Copyright (c) 2002 - 2012 Novell, Inc.
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail,
+# you may find current contact information at www.novell.com
+#
+# ***************************************************************************
+
+module Yast
+  class ChefWarningClient < Client
+    def main
+      Yast.import "Popup"
+      textdomain "base"
+
+      # Translators: a warning message in a continue-cancel question
+      # Opscode Chef is a different way to configure the system.
+      message = _(
+           "Chef Client is running. The changes that you make\n" +
+             "may be overridden by Chef later.\n" +
+             "Continue configuration with YaST?"
+      )
+      return Popup.ContinueCancel(message)
+    end
+  end
+end
+
+Yast::ChefWarningClient.new.main
