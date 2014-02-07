0993cde1613099d9c61a9e1ebb429a1f86043b69
Martin Vidner <mvidner@suse.cz>
2014-02-07 17:30:25 +0100

    improved rspec style much

--- library/general/test/asciifile_test.rb
+++ library/general/test/asciifile_test.rb
@@ -7,14 +7,6 @@ include Yast
 
 Yast.import "AsciiFile"
 
-FSTAB_FILENAME = "/etc/fstab"
-FSTAB_CONTENTS = <<EOS
-UUID=001c0d61-e99f-4ab7-ba4b-bda6f54a052d       /       btrfs   defaults 0 0
-# NFS
-192.168.1.1:/ /mnt    nfs4    rw 0 0
-# trailing comment
-EOS
-
 def stub_file_reading(filename, contents)
   SCR.stub(:Read)
     .with(path(".target.size"), filename)
@@ -26,25 +18,51 @@ def stub_file_reading(filename, contents)
 end
 
 describe "AsciiFile" do
-  describe "#ReadFile" do
-    it "does something" do
+  context "when working with a fstab file" do
+    FSTAB_FILENAME = "/etc/fstab"
+    FSTAB_CONTENTS = <<EOS
+UUID=001c0d61-e99f-4ab7-ba4b-bda6f54a052d       /       btrfs   defaults 0 0
+# NFS
+192.168.1.1:/ /mnt    nfs4    rw 0 0
+# trailing comment
+EOS
+    let(:fstab_ref) do
       stub_file_reading(FSTAB_FILENAME, FSTAB_CONTENTS)
       fstab = {}
       fstab_ref = arg_ref(fstab)
       AsciiFile.SetComment(fstab_ref, "^[ \t]*#")
       AsciiFile.SetDelimiter(fstab_ref, " \t")
       AsciiFile.SetListWidth(fstab_ref, [20, 20, 10, 21, 1, 1])
-      AsciiFile.ReadFile(fstab_ref, "/etc/fstab")
+      fstab_ref
+    end
 
-      expect(fstab["l"].size).to eq 4
+    describe "#ReadFile" do
+      before(:each) do
+        AsciiFile.ReadFile(fstab_ref, FSTAB_FILENAME)
+      end
 
-      # l: (lines) are a hash indexed by integers starting at ONE!
-      expect(fstab["l"][1]["fields"].size).to eq 6
-      # fields: an array starting at zero as usual
-      expect(fstab["l"][1]["fields"][1]).to eq "/"
+      # note that the result is `fstab["l"]`
+      # as the rest of `fstab` are the parsing parameters
+      subject(:result) { fstab_ref.value["l"] }
 
-      expect(fstab["l"][2]["comment"]).to be_true
-      expect(fstab["l"][2]["line"]).to eq "# NFS"
+      it "counts the lines" do
+        expect(result.size).to eq 4
+      end
+      it "indexes the lines, starting at ONE" do
+        expect(result.keys).to eq [1, 2, 3, 4]
+      end
+      it "counts the fields" do
+        expect(result[1]["fields"].size).to eq 6
+      end
+      it "indexes the fields, starting at zero as usual" do
+        expect(result[1]["fields"][1]).to eq "/"
+      end
+      it "recognizes comments" do
+        expect(result[2]["comment"]).to be_true
+      end
+      it "preserves the comment start" do
+        expect(result[2]["line"]).to eq "# NFS"
+      end
     end
   end
 end
