5ca5b0f9fb37ef11c6da5d9cdb0a6440bca8ee26
Martin Vidner <mvidner@suse.cz>
2014-02-17 13:41:52 +0100

    AsciiFile spec: better descriptions
    it is more fragmented now, like the result that it specifies :)

--- library/general/test/asciifile_test.rb
+++ library/general/test/asciifile_test.rb
@@ -43,25 +43,47 @@ EOS
 
       # note that the result is `fstab["l"]`
       # as the rest of `fstab` are the parsing parameters
-      subject(:result) { fstab_ref.value["l"] }
+      it "produces the result under the 'l' key" do
+        expect(fstab_ref.value).to have_key "l"
+      end
 
-      it "counts the lines" do
-        expect(result.size).to eq 4
-      end
-      it "indexes the lines, starting at ONE" do
-        expect(result.keys).to eq [1, 2, 3, 4]
-      end
-      it "counts the fields" do
-        expect(result[1]["fields"].size).to eq 6
-      end
-      it "indexes the fields, starting at zero as usual" do
-        expect(result[1]["fields"][1]).to eq "/"
-      end
-      it "recognizes comments" do
-        expect(result[2]["comment"]).to be_true
-      end
-      it "preserves the comment start" do
-        expect(result[2]["line"]).to eq "# NFS"
+      describe "the result" do
+        subject(:result) { fstab_ref.value["l"] }
+
+        it "is a hash indexed by line numbers, starting at ONE" do
+          expect(result.keys).to eq [1, 2, 3, 4]
+        end
+
+        describe "comment lines" do
+          subject (:comment) { result[2] }
+
+          it "have a true 'comment' key" do
+            expect(comment["comment"]).to be_true
+          end
+
+          it "have a copy in 'line' key, including the comment start" do
+            expect(comment["line"]).to eq "# NFS"
+          end
+        end
+
+        describe "regular non-comment lines" do
+          subject (:regular) { result[1] }
+
+          it "have a falsy 'comment' key" do
+            expect(regular["comment"]).to be_false
+          end
+
+          it "have a copy in 'line' key" do
+            expect(regular["line"]).to eq "UUID=001c0d61-e99f-4ab7-ba4b-bda6f54a052d       /       btrfs   defaults 0 0"
+          end
+
+          describe "its 'fields' key" do
+            it "is an array of delimiter separated fields, starting at zero as usual" do
+              expect(regular["fields"].size).to eq 6
+              expect(regular["fields"][1]).to eq "/"
+            end
+          end
+        end
       end
     end
   end
