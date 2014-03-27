105c3c688ac732130066840fc707a83758815077
Ladislav Slezák <lslezak@suse.cz>
2014-03-27 11:04:17 +0100

    fixed popup_test.rb, added test_helper.rb
    - 3.1.31

--- library/general/test/os_release_test.rb
+++ library/general/test/os_release_test.rb
@@ -1,8 +1,6 @@
 #! /usr/bin/env rspec
 
-top_srcdir = File.expand_path("../../../..", __FILE__)
-inc_dirs = Dir.glob("#{top_srcdir}/library/*/src")
-ENV["Y2DIR"] = inc_dirs.join(":")
+require File.expand_path("../test_helper.rb", __FILE__)
 
 require "yast"
 
