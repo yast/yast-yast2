747324664d6685de02dae729e28b56268d0e04fe
Gabriele Mohr <gs@suse.de>
2014-04-28 12:26:20 +0200

    add 'Stop' and 'Restart' for inst-sys (bnc #873120, bnc #873057)

--- library/systemd/src/modules/systemd_service.rb
+++ library/systemd/src/modules/systemd_service.rb
@@ -91,35 +91,6 @@ module Yast
       end
     end
 
-    class Service < SystemdUnit
-      include Yast::Logger
-
-      # Available only on installation system
-      START_SERVICE_INSTSYS_COMMAND = "/bin/service_start"
-
-      def pid
-        properties.pid
-      end
-
-      def running?
-        properties.running?
-      end
-
-      def start
-        if File.exist?(START_SERVICE_INSTSYS_COMMAND)
-          command = "#{START_SERVICE_INSTSYS_COMMAND} #{unit_name}"
-          log.info("Running command '#{command}'")
-          error.clear
-          result = OpenStruct.new(
-            SCR.Execute(Path.new(".target.bash_output"), command)
-          )
-          error << result.stderr
-          result.exit.zero?
-        else
-          super
-        end
-      end
-    end
   end
   SystemdService = SystemdServiceClass.new
 end
