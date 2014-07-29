274c78864b9d5e0fcd4d67d11ab53ced6f7c33e3
Josef Reidinger <jreidinger@suse.cz>
2014-07-29 09:57:42 +0200

    add example test for proc meminfo agent

---
+++ library/general/test/agents_test/proc_meminfo_agent_test.rb
@@ -0,0 +1,30 @@
+require "yast"
+
+describe ".proc.meminfo" do
+  before :each do
+    root = File.join(File.dirname(__FILE__), "test_root")
+    handle = Yast::WFM.SCROpen("chroot=#{root}:scr", false)
+    Yast::WFM.SCRSetDefault(handle)
+  end
+
+  after :each do
+    Yast::WFM.SCRClose(Yast::WFM.SCRGetDefault)
+  end
+
+  describe ".Read" do
+    it "read content of /proc/meminfo return hash" do
+      content = Yast::SCR.Read(Yast::Path.new(".proc.meminfo"))
+      expect(content).to be_a(Hash)
+    end
+
+    it "returned hash contain memtotal key" do
+      content = Yast::SCR.Read(Yast::Path.new(".proc.meminfo"))
+      expect(content).to include("memtotal" => 1021032)
+    end
+
+    it "returned hash contain memfree key" do
+      content = Yast::SCR.Read(Yast::Path.new(".proc.meminfo"))
+      expect(content).to include("memfree" => 83408)
+    end
+  end
+end
