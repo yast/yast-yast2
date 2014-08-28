05dc0fd272c3473d7e5b85a1f5c29f3751eecf85
Ancor Gonzalez Sosa <ancor@suse.de>
2014-08-28 13:39:44 +0200

    Added test for reading of .etc.xinetd_conf.services

--- library/general/test/agents_test/proc_meminfo_agent_test.rb
+++ library/general/test/agents_test/proc_meminfo_agent_test.rb
@@ -5,31 +5,27 @@ require "yast"
 
 describe ".proc.meminfo" do
 
-  AGENT_PATH = Yast::Path.new(".proc.meminfo")
   before :each do
     root = File.join(File.dirname(__FILE__), "test_root")
-    check_version = false
-    handle = Yast::WFM.SCROpen("chroot=#{root}:scr", check_version)
-    Yast::WFM.SCRSetDefault(handle)
+    set_root_path(root)
   end
 
   after :each do
-    Yast::WFM.SCRClose(Yast::WFM.SCRGetDefault)
+    reset_root_path
   end
 
   describe ".Read" do
+    let(:content) { Yast::SCR.Read(Yast::Path.new(".proc.meminfo")) }
+
     it "read content of /proc/meminfo return hash" do
-      content = Yast::SCR.Read(AGENT_PATH)
       expect(content).to be_a(Hash)
     end
 
     it "returned hash contain memtotal key" do
-      content = Yast::SCR.Read(AGENT_PATH)
       expect(content).to include("memtotal" => 1021032)
     end
 
     it "returned hash contain memfree key" do
-      content = Yast::SCR.Read(AGENT_PATH)
       expect(content).to include("memfree" => 83408)
     end
   end
