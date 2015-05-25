d0dbcc64a7985d0119a70637f3b60c46b0394091
Lukas Ocilka <lukas.ocilka@gmail.com>
2015-05-25 14:59:48 +0200

    Making RuboCop happy

--- library/network/test/susefirewall_proposal_test.rb
+++ library/network/test/susefirewall_proposal_test.rb
@@ -8,22 +8,24 @@ Yast.import "SuSEFirewallProposal"
 Yast.import "Linuxrc"
 
 describe Yast::SuSEFirewallProposal do
+  subject { Yast::SuSEFirewallProposal }
+
   describe "#ProposeFunctions" do
     context "when iscsi is used" do
       it "calls the iscsi proposal" do
         allow(Yast::Linuxrc).to receive(:useiscsi).and_return(true)
-        expect(Yast::SuSEFirewallProposal).to receive(:propose_iscsi).and_return(nil)
+        expect(subject).to receive(:propose_iscsi).and_return(nil)
 
-        Yast::SuSEFirewallProposal.ProposeFunctions
+        subject.ProposeFunctions
       end
     end
 
     context "when iscsi is not used" do
       it "does not call the iscsi proposal" do
         allow(Yast::Linuxrc).to receive(:useiscsi).and_return(false)
-        expect(Yast::SuSEFirewallProposal).not_to receive(:propose_iscsi)
+        expect(subject).not_to receive(:propose_iscsi)
 
-        Yast::SuSEFirewallProposal.ProposeFunctions
+        subject.ProposeFunctions
       end
     end
   end
@@ -32,11 +34,13 @@ describe Yast::SuSEFirewallProposal do
     it "proposes full firewall initialization on boot" do
       expect(Yast::SuSEFirewall).to receive(:full_init_on_boot).and_return(true)
 
-      Yast::SuSEFirewallProposal.propose_iscsi
+      subject.propose_iscsi
     end
   end
 
   describe "#EnableFallbackPorts" do
+    let(:fallback_ports) { ["port1", "port2"] }
+
     before(:each) do
       allow(Yast::SuSEFirewall).to receive(:GetKnownFirewallZones).and_return(["EXT", "INT", "DMZ"])
     end
@@ -45,15 +49,14 @@ describe Yast::SuSEFirewallProposal do
       it "opens given ports in firewall in given zones" do
         expect(Yast::SuSEFirewall).to receive(:AddService).with(/port.*/, "TCP", /(EXT|DMZ)/).exactly(4).times
 
-        Yast::SuSEFirewallProposal.EnableFallbackPorts(["port1","port2"], ["EXT", "DMZ"])
+        subject.EnableFallbackPorts(fallback_ports, ["EXT", "DMZ"])
       end
     end
 
     context "when opening ports in unknown firewall zones" do
       it "throws an exception" do
-        expect {
-          Yast::SuSEFirewallProposal.EnableFallbackPorts(["port1","port2"], ["UNKNOWN_ZONE1", "UZ2"])
-        }.to raise_error(/UNKNOWN_ZONE1.*UZ2/)
+        method_call = proc { subject.EnableFallbackPorts(fallback_ports, ["UNKNOWN_ZONE1", "UZ2"]) }
+        expect { method_call.call }.to raise_error(/UNKNOWN_ZONE1.*UZ2/)
       end
     end
   end
@@ -76,7 +79,7 @@ describe Yast::SuSEFirewallProposal do
     context "when network interfaces are assigned to some zone(s)" do
       it "open service in firewall in zones that include given interfaces" do
         expect(Yast::SuSEFirewall).to receive(:SetServicesForZones).with([firewall_service], interfaces_zones, true)
-        Yast::SuSEFirewallProposal.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
+        subject.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
       end
     end
 
@@ -84,22 +87,22 @@ describe Yast::SuSEFirewallProposal do
       it "opens service in firewall in all zones" do
         allow(Yast::SuSEFirewall).to receive(:GetZonesOfInterfaces).and_return([])
         expect(Yast::SuSEFirewall).to receive(:SetServicesForZones).with([firewall_service], all_zones, true)
-        Yast::SuSEFirewallProposal.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
+        subject.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
       end
     end
 
     context "when given firewall service is known" do
       it "opens service in firewall in zones that include given interfaces" do
         expect(Yast::SuSEFirewall).to receive(:SetServicesForZones).with([firewall_service], interfaces_zones, true)
-        Yast::SuSEFirewallProposal.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
+        subject.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
       end
     end
 
     context "when given service is unknown" do
       it "opens given fallback ports in zones that include given interfaces" do
         allow(Yast::SuSEFirewallServices).to receive(:IsKnownService).and_return(false)
-        expect(Yast::SuSEFirewallProposal).to receive(:EnableFallbackPorts).with(fallback_ports, interfaces_zones)
-        Yast::SuSEFirewallProposal.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
+        expect(subject).to receive(:EnableFallbackPorts).with(fallback_ports, interfaces_zones)
+        subject.OpenServiceInInterfaces(firewall_service, fallback_ports, network_interfaces)
       end
     end
   end
