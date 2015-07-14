62355f1b6d05c12eea4a6bcc536b14e270f5b67a
Josef Reidinger <jreidinger@suse.cz>
2015-07-14 14:31:01 +0200

    add more user friendly way to access augeas tree

--- library/general/src/lib/config_file/augeas_parser.rb
+++ library/general/src/lib/config_file/augeas_parser.rb
@@ -1,59 +1,106 @@
 require "augeas"
 
 module ConfigFile
+
+  # Represent parsed augeas config tree with user friendly methods
+  class AugeasTree
+    def initialize
+      @data = {}
+    end
+
+    def [](key)
+      entry = @data.find{|d| d[:key] == key}
+      return nil unless entry
+
+      entry[:value]
+    end
+
+    def []=(key, value)
+      entry = @data.find{|d| d[:key] == key}
+      if entry
+        entry[:value] = value
+      else
+        @data << {
+          key:   key,
+          value: value
+        }
+      end
+    end
+
+    def load_from_augeas(aug, prefix)
+      matches = aug.match("#{prefix}/*")
+
+      @data = matches.map do |aug_key|
+        key = aug_key.sub(/^#{prefix}\//, "")
+        key.sub!(/\[\d+\]/, "[]")
+        {
+          key:   key,
+          value: aug.get(aug_key)
+        }
+      end
+    end
+
+    def save_to_augeas(aug, prefix)
+      arrays = {}
+
+      @data.each do |entry|
+        key = entry[:key]
+        if key.end_with?("[]")
+          array_key = key.sub(/\[\]$/, "")
+          arrays[array_key] ||= 0
+          arrays[array_key] += 1
+          key = array_key + "[#{arrays[array_key]}]"
+        end
+        aug.set("#{prefix}/#{key}", entry[:value])
+      end
+    end
+  end
+
   # TODO: handle nested structures
-  # TODO: use for data smarted structure that can search
   # @example read, print, modify and serialize again
+  #    require "config_file/augeas_parser"
   #
-  #  parser = ConfigFile::AugeasParser.new("sysconfig.lns")
-  #  data = parser.parse(File.read("/etc/default/grub"))
-  #  puts data
-  #  data.find { |d| d[:key] == "GRUB_DISABLE_OS_PROBER" }[:value] = "true"
-  #  puts parser.serialize(data)
+  #    parser = ConfigFile::AugeasParser.new("sysconfig.lns")
+  #    data = parser.parse(File.read("/etc/default/grub"))
+  #
+  #    puts data["GRUB_DISABLE_OS_PROBER"]
+  #    data["GRUB_DISABLE_OS_PROBER"] = "true"
+  #    puts parser.serialize(data)
   class AugeasParser
-    def initialize(lense)
-      @lense = lense
+    def initialize(lens)
+      @lens = lens
     end
 
     def parse(raw_string)
       @old_content = raw_string
 
-      Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
+      # open augeas without any autoloading and it should not touch disk and
+      # load lenses as needed only
+      root = load_path = nil
+      Augeas::open(root, load_path, Augeas::NO_MODL_AUTOLOAD) do |aug|
         aug.set("/input", raw_string)
-        if !aug.text_store(@lense, "/input", "/store")
+        if !aug.text_store(@lens, "/input", "/store")
           error = aug.error
           raise "Augeas error #{error[:message]}. Details: #{error[:details]}."
         end
-        matches = aug.match("/store/*")
 
-        return matches.map do |aug_key|
-          key = aug_key.sub(/^\/store\//, "")
-          key.sub!(/\[\d+\]/, "[]")
-          {
-            key:   key,
-            value: aug.get(aug_key)
-          }
-        end
+        tree = AugeasTree.new
+        tree.load_from_augeas(aug, "/store")
+
+        return tree
       end
     end
 
     def serialize(data)
-      arrays = {}
 
-      Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
+      # open augeas without any autoloading and it should not touch disk and
+      # load lenses as needed only
+      root = load_path = nil
+      Augeas::open(root, load_path, Augeas::NO_MODL_AUTOLOAD) do |aug|
         aug.set("/input", @old_content || "")
-        data.each do |entry|
-          key = entry[:key]
-          if key.end_with?("[]")
-            array_key = key.sub(/\[\]$/, "")
-            arrays[array_key] ||= 0
-            arrays[array_key] += 1
-            key = array_key + "[#{arrays[array_key]}]"
-          end
-          aug.set("/store/#{key}", entry[:value])
-        end
+        data.save_to_augeas(aug, "/store")
 
-        aug.text_retrieve(@lense, "/input", "/store", "/output")
+        aug.text_retrieve(@lens, "/input", "/store", "/output")
 
         return aug.get("/output")
       end
