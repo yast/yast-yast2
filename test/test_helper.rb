0b39fcecb3b48b0dc6c1ae4c6b4e7e6b1f81abf8
Martin Vidner <mvidner@suse.cz>
2015-07-14 09:04:40 +0200

    Fail tests if mocked method does not exist
    If you misremember a method name both in code and in tests,
    will save you.
    https://relishapp.com/rspec/rspec-mocks/v/3-0/docs/verifying-doubles/partial-doubles

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,19 @@ ENV["Y2DIR"] = inc_dirs.join(":")
 require "yast"
 require "yast/rspec"
 
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    # If you misremember a method name both in code and in tests,
+    # will save you.
+    # https://relishapp.com/rspec/rspec-mocks/v/3-0/docs/verifying-doubles/partial-doubles
+    #
+    # With graceful degradation for RSpec 2
+    if mocks.respond_to?(:verify_partial_doubles=)
+      mocks.verify_partial_doubles = true
+    end
+  end
+end
+
 if ENV["COVERAGE"]
   require "simplecov"
   SimpleCov.start
