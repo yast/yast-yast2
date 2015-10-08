36621ba18a944301830393dcb01c80cd15c81408
Josef Reidinger <jreidinger@suse.cz>
2015-10-08 12:52:10 +0200

    improvement from review

--- library/packages/src/modules/PackageCallbacks.rb
+++ library/packages/src/modules/PackageCallbacks.rb
@@ -3052,7 +3052,7 @@ module Yast
     def autoeject
       load_config unless @config
 
-      @config["automatic_eject"] || false
+      @config.fetch("automatic_eject", false)
     end
 
     def store_autoeject(value)
