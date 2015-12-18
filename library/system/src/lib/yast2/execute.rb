6a8c700badecb1339b29a2cd7ce9e1ef81204f5f
Josef Reidinger <jreidinger@suse.cz>
2015-12-18 16:39:02 +0100

    add support for running programs without SCR and also for using config_files_api gem

---
+++ library/system/src/lib/yast2/execute.rb
@@ -0,0 +1,60 @@
+# ***************************************************************************
+#
+# Copyright (c) 2015 SUSE LLC
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail,
+# you may find current contact information at www.novell.com
+#
+# ***************************************************************************
+
+require "yast"
+require "cheetah"
+
+Yast.import "installation"
+
+module Yast2
+  # Module for executing scripts/programs in safe way. Uses cheetah as backend,
+  # but adds support for chrooting in installation.
+  class Execute
+    # use y2log by default
+    Cheetah.default_options = { logger: Y2Logger.instance }
+
+    class << self
+      # Runs arguments with respect of changed root in installation.
+      # @see Cheetah.run for parameters
+      # @raise Cheetah::ExecutionFailed
+      def on_target(*args)
+        root = "/"
+        root = Yast::Installation.destdir if Yast::WFM.scr_chrooted?
+
+        if args.last.is_a? Hash
+          args.last[:chroot] = root
+        else
+          args.push(chroot: root)
+        end
+
+        Cheetah.run(*args)
+      end
+
+      # Runs arguments without changed root.
+      # @see Cheetah.run for parameters
+      # @raise Cheetah::ExecutionFailed
+      def locally(*args)
+        Cheetah.run(*args)
+      end
+    end
+  end
+end
---
+++ library/system/src/lib/yast2/target_file.rb
@@ -0,0 +1,48 @@
+# ***************************************************************************
+#
+# Copyright (c) 2015 SUSE LLC
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail,
+# you may find current contact information at www.novell.com
+#
+# ***************************************************************************
+
+require "yast"
+
+Yast.import "installation"
+
+module Yast2
+  # Module provides file class that can be used for config_files_api gem.
+  class TargetFile
+    # Reads file content with respect of changed root in installation.
+    def self.read(path)
+      ::File.read(final_path(path))
+    end
+
+    # Writes file content with respect of changed root in installation.
+    def self.write(path, content)
+      ::File.write(final_path(path), content)
+    end
+
+    def self.final_path(path)
+      root = "/"
+      root = Yast::Installation.destdir if Yast::WFM.scr_chrooted?
+
+      File.join(root, path)
+    end
+    private_class_method :final_path
+  end
+end
