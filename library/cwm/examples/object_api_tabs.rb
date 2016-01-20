c58852613d96d49df09b2ba62246ec42ce262351
Josef Reidinger <jreidinger@suse.cz>
2016-01-20 14:45:28 +0100

    add tabs widgets together with example usage

---
+++ library/cwm/examples/object_api_tabs.rb
@@ -0,0 +1,153 @@
+# Simple example to demonstrate object API for CWM
+
+require_relative "example_helper"
+
+require "cwm/widget"
+
+Yast.import "CWM"
+Yast.import "Popup"
+Yast.import "Wizard"
+
+class LuckyNumberWidget < ::CWM::IntField
+  attr_reader :result, :minimum, :maximum
+
+  def initialize
+    @minimum = 0
+    @maximum = 1000
+  end
+
+  def label
+    _("Lucky number")
+  end
+
+  def init
+    self.value = @result if @result
+  end
+
+  def store
+    @result = value
+  end
+end
+
+class GenerateButton < ::CWM::PushButton
+  def initialize(lucky_number_widget)
+    @lucky_number_widget = lucky_number_widget
+  end
+
+  def label
+    _("Generate Lucky Number")
+  end
+
+  def handle
+    @lucky_number_widget.value = rand(1000)
+
+    nil
+  end
+end
+
+class LuckyNumberTab < ::CWM::Tab
+  def initialize
+    self.initial = true
+  end
+
+  def contents
+    HBox(
+      button_widget,
+      lucky_number_widget
+    )
+  end
+
+  def result
+    lucky_number_widget.value
+  end
+
+  def label
+    _("Lucky Number")
+  end
+
+private
+
+  def button_widget
+    @button_widget ||= GenerateButton.new(lucky_number_widget)
+  end
+
+  def lucky_number_widget
+    @lucky_number_widget ||= LuckyNumberWidget.new
+  end
+end
+
+class TrueLoveSelector < ::CWM::RadioButtons
+  def initialize
+    @chosen = nil
+  end
+
+  def label
+    _("Select true love")
+  end
+
+  def items
+    [
+      [:human, "Human"],
+      [:pc, "PC"]
+    ]
+  end
+
+  def init
+    self.value = @chosen if @chosen
+  end
+
+  def validate
+    if value == :pc
+      Yast::Popup.Error(_("Human will be exterminated, pc cannot allow you to be your true love"))
+      return false
+    end
+
+    true
+  end
+
+  def store
+    @chosen = value
+  end
+end
+
+class TrueLoveTab < ::CWM::Tab
+  def contents
+    HBox(
+      true_love_selector
+    )
+  end
+
+  def label
+    _("True Love")
+  end
+
+private
+
+  def true_love_selector
+    @true_love_selector ||= TrueLoveSelector.new
+  end
+end
+
+module Yast
+  class ExampleDialog
+    include Yast::I18n
+    include Yast::UIShortcuts
+
+    def run
+      textdomain "example"
+
+      lucky_number_tab = LuckyNumberTab.new
+      true_love_tab = TrueLoveTab.new
+
+      tabs = ::CWM::Tabs.new(lucky_number_tab, true_love_tab)
+
+      contents = VBox(tabs)
+
+      Yast::Wizard.CreateDialog
+      CWM.show(contents, caption: _("Tabs example"))
+      Yast::Wizard.CloseDialog
+    end
+  end
+end
+
+Yast::ExampleDialog.new.run
--- library/cwm/src/lib/cwm/widget.rb
+++ library/cwm/src/lib/cwm/widget.rb
@@ -299,7 +299,7 @@ module CWM
       Yast::CWM.widgets_in_contents(contents)
     end
 
-  private
+  protected
 
     def cwm_contents
       Yast.import "CWM"
@@ -642,5 +642,184 @@ module CWM
     self.widget_type = :richtext
 
     include ValueBasedWidget
+  end
+
+  # Tab widget, usefull only with {CWM::Tabs}
+  # @see tabs example for usage
+  class Tab < CustomWidget
+    attr_accessor :initial
+
+    # content of tab, can contain another widgets
+    abstract_method :contents
+
+    def cwm_definition
+      super.merge(
+        "widgets" => cwm_widgets,
+        "custom_widget" => Yast::CWM.PrepareDialog(cwm_contents, cwm_widgets)
+      )
+    end
+
+    # get cwm style of widget definitions
+    # @note internal api only used as gate to communicate with CWM
+    def cwm_widgets
+      return @cwm_widgets if @cwm_widgets
+
+      widgets = nested_widgets
+      names = widgets.map(&:widget_id)
+      definition = Hash[widgets.map { |w| [ w.widget_id, w.cwm_definition ] }]
+      @cwm_widgets = Yast::CWM.CreateWidgets(names, definition)
+    end
+
+  protected
+
+    # defines name of tab header
+    # It is recommended to rewrite it
+    def label
+      textdomain "base"
+
+      _("Tab")
+    end
+
+    # help that is result of used widget helps.
+    # If overwritting, do not forget to use `super`, otherwise widget helps will
+    # be missing
+    def help
+      Yast::CWM.MergeHelps(nested_widgets.map(&:cwm_definition))
+    end
+  end
+
+  # useful to have tabs as widget. It contained {CWM::Tab} with its content
+  # @see examples/object_api_tabs.rb
+  class Tabs < CustomWidget
+    # param [Array<CWM::Tab>] tabs to be shown
+    def initialize(*tabs)
+      @tabs = tabs
+      @current_tab = nil
+      self.handle_all_events = true
+    end
+
+    # gets visual order of tabs
+    # This default implementation returns same order as passed to constructor
+    def tab_order
+      @tabs.map(&:widget_id)
+    end
+
+    # initializes tabs, show tab which is initial
+    def init
+      switch_tab(initial_tab)
+    end
+
+    def handle(event)
+      # pass it to content of tab at first, maybe something stop passing
+      res = Yast::CWM.handleWidgets(@current_tab.cwm_widgets, event)
+      return res if res
+
+      new_id = event["ID"]
+      tab = tab_for_id(new_id)
+
+      return nil unless tab
+
+      return nil if @current_tab.widget_id == new_id
+
+      unless validate
+        mark_tab(@current_tab)
+        return nil
+      end
+
+      store_tab(@current_tab.widget_id)
+
+      switch_tab(new_id)
+
+      nil
+    end
+
+    # store content of current tab
+    def store
+      store_tab(@current_tab.widget_id)
+    end
+
+    # validates current tab
+    def validate
+      Yast::CWM.validateWidgets(@current_tab.cwm_definition["widgets"], {"ID" => @current_tab.widget_id})
+    end
+
+    # stores tab with given id
+    def store_tab(tab_id)
+      Yast::CWM.saveWidgets(tab_for_id(tab_id).cwm_definition["widgets"], {"ID" => tab_id})
+    end
+
+  protected
+
+    # switch to target tab
+    def switch_tab(tab_id)
+      tab = tab_for_id(tab_id)
+      return unless tab
+
+      mark_tab(tab)
+      Yast::UI.ReplaceWidget(Id(replace_point_id), tab.cwm_definition["custom_widget"])
+      Yast::CWM.initWidgets(tab.cwm_definition["widgets"])
+      @current_tab = tab
+    end
+
+    # visually mark currently active tab
+    def mark_tab(tab)
+      if Yast::UI.HasSpecialWidget(:DumbTab)
+        Yast::UI.ChangeWidget(Id(widget_id), :CurrentItem, tab.widget_id)
+      else
+        if @current_tab
+          Yast::UI.ChangeWidget(
+            Id(@current_tab.widget_id),
+            :Label,
+            @current_tab.label
+          )
+        end
+        Yast::UI.ChangeWidget(
+          Id(tab.widget_id),
+          :Label,
+          "#{Yast::UI.Glyph(:BulletArrowRight)}  #{tab.label}"
+        )
+      end
+    end
+
+    # gets id of initial tab
+    # This default implementation returns first tab from {#tabs} method
+    def initial_tab
+      initial = @tabs.find(&:initial)
+
+      (initial || @tabs.first).widget_id
+    end
+
+    def contents
+      textdomain "base"
+
+      if Yast::UI.HasSpecialWidget(:DumbTab)
+        panes = tab_order.map do |tab_id|
+          tab = tab_for_id(tab_id)
+          Item(Id(tab.widget_id), tab.label, tab.widget_id == initial_tab)
+        end
+        DumbTab(Id(widget_id), panes, replace_point)
+      else
+        tabbar = HBox()
+        tabbar = tab_order.each_with_object(HBox()) do |tab, res|
+          tab = tab_for_id(tab)
+          tabbar << PushButton(Id(tab.widget_id), tab.label)
+        end
+        VBox(Left(tabbar), Frame("", replace_point))
+      end
+    end
+
+    def tab_for_id(id)
+      @tabs.find { |t| t.widget_id == id }
+    end
+
+  private
+
+    def replace_point_id
+      :_cwm_tab_contents_rp
+    end
+
+    def replace_point
+      ReplacePoint(Id(replace_point_id), VBox(VStretch(), HStretch()))
+    end
   end
 end
