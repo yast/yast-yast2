fbd230951875eab4e25014f37bba2cc191ac52db
Martin Vidner <mvidner@suse.cz>
2016-01-21 16:59:27 +0100

    Do not count tests themselves in test coverage.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,7 +28,9 @@ end
 
 if ENV["COVERAGE"]
   require "simplecov"
-  SimpleCov.start
+  SimpleCov.start do
+    add_filter "/test/"
+  end
   # for coverage we need to load all ruby files
   Dir["#{root_location}/library/*/src/{module,lib}/**/*.rb"].each { |f| require_relative f }
   # use coveralls for on-line code coverage reporting at Travis CI
