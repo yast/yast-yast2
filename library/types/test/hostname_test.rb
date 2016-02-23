1ea7e0bbed125ea514008336e622e0ff47e4f414
Michal Filka <mfilka@suse.cz>
2016-02-23 11:44:42 +0100

    Updated testsuite for Hostname

--- library/types/test/hostname_test.rb
+++ library/types/test/hostname_test.rb
@@ -10,30 +10,49 @@ describe "Hostname#CurrentFQ" do
 
   let(:etc_hostname) { "etc.hostname.cz" }
   let(:cmd_hostname) { "cmd.hostname.cz" }
+  let(:hostname)     { Yast::Hostname }
 
-  it "returns output of hostname --fqdn if available" do
+  def allow_execute_hostname(stdout, code = 0, stderr = "")
+    ret = { "stdout" => stdout, "stderr" => stderr, "exit" => code }
+
     allow(Yast::SCR)
       .to receive(:Execute)
       .with(path(".target.bash_output"), "hostname --fqdn")
-      .and_return("stdout" => cmd_hostname, "exit" => 0)
-
-    expect(Yast::Hostname.CurrentFQ).to eq cmd_hostname
+      .and_return(ret)
   end
 
-  it "reads /etc/hostname when hostname --fqdn fails" do
-    allow(Yast::SCR)
-      .to receive(:Execute)
-      .with(path(".target.bash_output"), "hostname --fqdn")
-      .and_return(nil)
+  def allow_read_hostname(result)
     allow(Yast::SCR)
       .to receive(:Read)
       .with(path(".target.string"), "/etc/hostname")
-      .and_return(etc_hostname)
+      .and_return(result)
+  end
+
+  it "returns output of hostname --fqdn if available" do
+    allow_execute_hostname(cmd_hostname)
+
+    expect(hostname.CurrentFQ).to eq cmd_hostname
+  end
+
+  it "reads /etc/hostname when hostname --fqdn fails" do
+    allow_execute_hostname("", 1)
+    allow_read_hostname(etc_hostname)
+
     allow(Yast::FileUtils)
       .to receive(:Exists)
       .with("/etc/hostname")
       .and_return(true)
 
-    expect(Yast::Hostname.CurrentFQ).to eq etc_hostname
+    expect(hostname.CurrentFQ).to eq etc_hostname
+  end
+
+  it "returns default hostname proposal when generating AY profile at the end of installation" do
+    Yast.import "Stage"
+
+    allow_execute_hostname("1.1.1.1")                     # linuxrc sometimes provides IP as hostname
+    allow_read_hostname("")                                  # linuxrc do not provide /etc/hostname
+    allow(Yast::Stage).to receive(:initial).and_return(true) # and all this happens only in installer
+
+    expect(hostname.CurrentFQ).to eq "linux.#{hostname.DefaultDomain}"
   end
 end
