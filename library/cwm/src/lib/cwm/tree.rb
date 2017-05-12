d85d39be163cc335d538e16cd164db1816776807
Martin Vidner <martin@vidner.net>
2017-05-12 18:02:36 +0200

    WIP: can construct it with proper TreeItems but broke content switching

--- library/cwm/src/lib/cwm/tree.rb
+++ library/cwm/src/lib/cwm/tree.rb
@@ -1,42 +1,48 @@
 module CWM
+  class TreeItem
+    attr_reader :id, :label, :icon, :open, :data
+    # @return [Hash{id => TreeItem}]
+    attr_reader :children
+
+    def initialize(id, label, icon: nil, open: true, data: nil, children: {})
+      @id = id
+      @label = label
+      @icon = icon
+      @open = open
+      @data = data
+      @children = children
+    end
+
+    def ui_term
+      args = [Id(id)]
+      args << Yast::Term.new(:icon, icon) if icon
+      args << label
+      args << open
+      args << children.values.map(&:ui_term)
+      Item(*args)
+    end
+  end
+
   class Tree < Tabs
     def contents
-      tree = Tree(Id(widget_id), Opt(:notify), "TODO Tree title", items)
+      item_terms = items.map { |id, i| i.ui_term }
+      tree = Tree(Id(widget_id), Opt(:notify), label, item_terms)
       HBox(
         HWeight(30, tree),
         HWeight(70, replace_point)
       )
     end
 
-    private
-
-    def tab_for_id(id)
-      if id == widget_id
-        id = Yast::UI.QueryWidget(Id(id), :CurrentBranch).last
-      end
-      super(id)
+  private
+    def init
+      # nothing, dont select initial "tab" yet
     end
 
-    def items
-      # FIXME too simple
-      panes = tab_order.map do |tab_id|
-        tab = tab_for_id(tab_id)
-        item_for(tab.widget_id, tab.label)
-      end
-      panes
-    end
+    # Subclass will override
+    def label; "?"; end
 
-    def item_for(id, title, icon: nil, subtree: [])
-      args = [Id(id)]
-      args << Yast::Term.new(:icon, icon) if icon
-      args << title
-      args << open?(id)
-      args << subtree
-      Item(*args)
-    end
-
-    def open?(id)
-      id == initial_tab_id
-    end
+    # Subclass will override
+    # Hash
+    def items; {}; end
   end
 end
