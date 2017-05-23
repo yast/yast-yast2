c3f06e0afc3331dd1cf0ab6dbf60aced87930974
Martin Vidner <martin@vidner.net>
2017-05-23 15:58:11 +0200

    Split up CWM::Tabs to CWM::PushButtonTabPager and CWM::DumbTabPager

--- library/cwm/src/lib/cwm/tabs.rb
+++ library/cwm/src/lib/cwm/tabs.rb
@@ -9,43 +9,68 @@ module CWM
 
   # @see examples/object_api_tabs.rb
   class Tabs < Pager
+    # {Tabs} does not have instances:
+    # {Tabs.new} overrides {Class.new} and calls
+    # either {DumbTabPager.new} or {PushButtonTabPager.new}.
+    def self.new(*args)
+      if Yast::UI.HasSpecialWidget(:DumbTab)
+        DumbTabPager.new(*args)
+      else
+        PushButtonTabPager.new(*args)
+      end
+    end
+  end
+
+  # A {Pager} for the GUI, using the DumbTab widget
+  class DumbTabPager < Pager
+  #
+
   protected
 
     # visually mark currently active tab
     # @param page [Page]
     def mark_page(page)
-      if Yast::UI.HasSpecialWidget(:DumbTab)
-        Yast::UI.ChangeWidget(Id(widget_id), :CurrentItem, page.widget_id)
-      else
-        if @current_page
-          Yast::UI.ChangeWidget(
-            Id(@current_page.widget_id),
-            :Label,
-            @current_page.label
-          )
-        end
-        Yast::UI.ChangeWidget(
-          Id(page.widget_id),
-          :Label,
-          "#{Yast::UI.Glyph(:BulletArrowRight)}  #{page.label}"
-        )
-      end
+      Yast::UI.ChangeWidget(Id(widget_id), :CurrentItem, page.widget_id)
     end
 
     def contents
-      if Yast::UI.HasSpecialWidget(:DumbTab)
-        panes = page_order.map do |page_id|
-          page = page_for_id(page_id)
-          Item(Id(page.widget_id), page.label, page.widget_id == initial_page_id)
-        end
-        DumbTab(Id(widget_id), panes, replace_point)
-      else
-        tabbar = page_order.each_with_object(HBox()) do |page, res|
-          page = page_for_id(page)
-          res << PushButton(Id(page.widget_id), page.label)
-        end
-        VBox(Left(tabbar), Frame("", replace_point))
+      panes = page_order.map do |page_id|
+        page = page_for_id(page_id)
+        Item(Id(page.widget_id), page.label, page.widget_id == initial_page_id)
       end
+      DumbTab(Id(widget_id), panes, replace_point)
+    end
+  end
+
+  # A {Pager} for ncurses, using PushButtons to simulate the tabs
+  class PushButtonTabPager < Pager
+  #
+
+  protected
+
+    # visually mark currently active tab
+    # @param page [Page]
+    def mark_page(page)
+      if @current_page
+        Yast::UI.ChangeWidget(
+          Id(@current_page.widget_id),
+          :Label,
+          @current_page.label
+        )
+      end
+      Yast::UI.ChangeWidget(
+        Id(page.widget_id),
+        :Label,
+        "#{Yast::UI.Glyph(:BulletArrowRight)}  #{page.label}"
+      )
+    end
+
+    def contents
+      tabbar = page_order.each_with_object(HBox()) do |page, res|
+        page = page_for_id(page)
+        res << PushButton(Id(page.widget_id), page.label)
+      end
+      VBox(Left(tabbar), Frame("", replace_point))
     end
   end
 end
