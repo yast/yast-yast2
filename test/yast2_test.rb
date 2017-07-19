b5709f859744022d730b2a8e7e6254035d3119d5
Imobach González Sosa <imobachgs@gmail.com>
2017-07-19 09:18:42 +0100

    Add y2log to system logs master (#599)
    * adapt Rakefile and Dockerfile for SLE-12-SP3
    * Add y2log to system logs (#597)
    
    * Add y2log to the list of files in view_anymsg client
    * Bump version and update changes file
    
    * Avoid side effects in yast2_test.rb
    
    * Revert "adapt Rakefile and Dockerfile for SLE-12-SP3"
    
    This reverts commit c5a4384c70fcb9bc53023b640d360c79372e6e3c.

--- test/yast2_test.rb
+++ test/yast2_test.rb
@@ -4,12 +4,18 @@ require_relative "test_helper"
 
 require "cheetah"
 
-additional_y2dir = File.expand_path("../test_y2dir", __FILE__)
-ENV["Y2DIR"] += ":#{additional_y2dir}"
 TEST_DIR = File.expand_path("../../scripts", __FILE__)
 
 describe "yast2 script" do
-  it "pass properly all arguments" do
+  around do |example|
+    old_y2dir = ENV["Y2DIR"]
+    additional_y2dir = File.expand_path("../test_y2dir", __FILE__)
+    ENV["Y2DIR"] = ENV["Y2DIR"] + ":#{additional_y2dir}"
+    example.run
+    ENV["Y2DIR"] = old_y2dir
+  end
+
+  it "passes properly all arguments" do
     Cheetah.run(TEST_DIR + "/yast2", "args_test_client", 'abc"\'\\|;&<>! ', "second", env: { "TESTING_YAST2" => "1" })
   end
 end
