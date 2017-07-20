7323293473711eb306ba925ae484ee5f282483b5
Martin Vidner <mvidner@suse.cz>
2017-07-20 11:07:45 +0200

    More precise changelog and tests

--- library/cwm/test/wrapper_widget_test.rb
+++ library/cwm/test/wrapper_widget_test.rb
@@ -7,20 +7,20 @@ require "cwm/wrapper_widget"
 describe CWM::WrapperWidget do
   describe "#cwm_definition" do
     it "returns passed hash content" do
-      content = { "test" => "test" }
-      expect(described_class.new(content).cwm_definition["test"]).to eq "test"
+      content = { "test" => "value" }
+      expect(described_class.new(content).cwm_definition["test"]).to eq "value"
     end
 
     it "return hash that has _cwm_key key with widget id" do
-      content = { "test" => "test" }
-      expect(described_class.new(content, id: "test").cwm_definition["_cwm_key"]).to eq "test"
+      content = { "test" => "value" }
+      expect(described_class.new(content, id: "wid").cwm_definition["_cwm_key"]).to eq "wid"
     end
   end
 
   describe "#widget_id" do
     it "returns passed widget id" do
-      content = { "test" => "test" }
-      expect(described_class.new(content, id: "test").widget_id).to eq "test"
+      content = { "test" => "value" }
+      expect(described_class.new(content, id: "wid").widget_id).to eq "wid"
     end
   end
 end
