829d92e6f258ffcb963425097ff8b438e110ca01
Imobach González Sosa <igonzalezsosa@suse.com>
2017-08-11 13:26:16 +0100

    Move missing parts from Installation::Product to Packages::Product

--- library/packages/test/lib/product_test.rb
+++ library/packages/test/lib/product_test.rb
@@ -15,7 +15,6 @@ describe Packages::Product do
   end
 
   describe "==" do
-
     context "when name, arch, version and vendor match" do
       let(:other) { Packages::Product.new(BASE_ATTRS) }
 
@@ -53,6 +52,62 @@ describe Packages::Product do
 
       it "returns false" do
         expect(subject == other).to eq(false)
+      end
+    end
+  end
+
+  describe "#selected?" do
+    before do
+      allow(Yast::Pkg).to receive(:ResolvableProperties).with(product.name, :product, "")
+        .and_return([{ "name" => product.name, "status" => status }])
+    end
+
+    context "if product was selected for installation" do
+      let(:status) { :selected }
+
+      it "returns true" do
+        expect(product).to be_selected
+      end
+    end
+
+    context "if product was not selected for installation" do
+      let(:status) { :none }
+
+      it "returns false" do
+        expect(product).to_not be_selected
+      end
+    end
+  end
+
+  describe "#select" do
+    it "selects the product for installation" do
+      expect(Yast::Pkg).to receive(:ResolvableInstall).with(product.name, :product, "")
+      product.select
+    end
+  end
+
+  describe "#label" do
+    context "when 'display_name' is present" do
+      subject(:product) { Packages::Product.new(name: "NAME", display_name: "DISPLAY", short_name: "SHORT") }
+
+      it "returns 'display_name'" do
+        expect(product.label).to eq("DISPLAY")
+      end
+    end
+
+    context "when 'display_name' is not present" do
+      subject(:product) { Packages::Product.new(name: "NAME", short_name: "SHORT") }
+
+      it "returns 'short_name'" do
+        expect(product.label).to eq("SHORT")
+      end
+    end
+
+    context "when 'display_name' nor 'short_name' are present" do
+      subject(:product) { Packages::Product.new(name: "NAME") }
+
+      it "returns 'name'" do
+        expect(product.label).to eq("NAME")
       end
     end
   end
