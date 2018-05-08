d3a8d6d5b2a681c56504f3b5d29ffb01a6443018
José Iván López González <jlopez@suse.com>
2018-05-08 11:55:40 +0100

    Add back and abort default handlers to CWM::Dialog

--- library/cwm/test/dialog_test.rb
+++ library/cwm/test/dialog_test.rb
@@ -15,6 +15,12 @@ describe "CWM::Dialog" do
   include_examples "CWM::Dialog"
 
   describe ".run" do
+    before do
+      allow(Yast::Wizard).to receive(:IsWizardDialog).and_return(false)
+      allow(Yast::Wizard).to receive(:CreateDialog)
+      allow(Yast::Wizard).to receive(:CloseDialog)
+    end
+
     it "opens a dialog when needed, and calls CWM#show" do
       expect(Yast::Wizard).to receive(:IsWizardDialog).and_return(false)
       expect(Yast::Wizard).to receive(:CreateDialog)
@@ -31,6 +37,24 @@ describe "CWM::Dialog" do
       expect(Yast::CWM).to receive(:show).and_return(:launch)
 
       expect(subject.class.run).to eq(:launch)
+    end
+
+    it "uses the back handler" do
+      expect(Yast::CWM).to receive(:show) do |_content, options|
+        expect(options).to include(:back_handler)
+        expect(options[:back_handler].call).to eq(true)
+      end
+
+      subject.class.run
+    end
+
+    it "uses the abort handler" do
+      expect(Yast::CWM).to receive(:show) do |_content, options|
+        expect(options).to include(:abort_handler)
+        expect(options[:abort_handler].call).to eq(true)
+      end
+
+      subject.class.run
     end
   end
 end
