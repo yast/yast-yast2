196bc0025bb3c6b124a76c5af6c0443a349477f1
Josef Reidinger <jreidinger@suse.cz>
2018-06-28 17:28:59 +0200

    implementation WIP

--- library/systemd/src/lib/yast2/service_configuration.rb
+++ library/systemd/src/lib/yast2/service_configuration.rb
@@ -1,3 +1,7 @@
+require "yast"
+
+Yast.import "SystemdService"
+
 module Yast2
   # Class that holds configuration for single or multiple services. It allows
   # read current status from system. It allows to modify target state. And it
@@ -5,6 +9,8 @@ module Yast2
   # Its common usage is with {Yast2::ServiceWidget} which allows to modify
   # configuration and write it when user approve whole dialog.
   class ServiceConfiguration
+    include Yast::Logger
+
     # services managed by this configuration
     attr_reader :services
 
@@ -16,11 +22,23 @@ module Yast2
     # @example three services that wants reload instead of restart
     #   config = Yast2::ServiceConfiguration.new(s1, s2, s3, reload: true)
     def initialize(*services, reload: false)
+      if services.any? { |s| !s.is_a?(Yast::SystemdServiceClass::Service) }
+        raise ArgumentError, "Services can be only Systemd Service - #{services.inspect}"
+      end
+
+      @services = services
+      @reload = reload
     end
 
     # reads system status of services. Can be also used to reread current status.
     # @raise <Yast::SystemctlError> when read services state failed
     def read
+      # safe initial state when exception happen
+      @status = :unknown
+      @autostart = :unknown
+
+      read_status
+      read_autostart
     end
 
     # writes services new status
@@ -37,7 +55,10 @@ module Yast2
     #   - `:inconsistent` when part of services is active and part not.
     #       Can happen only when configuration handle multiple services
     #   - `:unknown` when read of current status failed.
-    def current_status
+    def status
+      read unless @status
+
+      @status
     end
 
     # returns currently set target action. If it is not yet set it returns
@@ -49,13 +70,16 @@ module Yast2
     #   - `:restart` restart all services. Can be reload if specified during
     #       construction of this class. If service is inactive, it is started.
     #   - `:nothing` do not touch anything.
-    def target_action
+    def action
+      return :nothing unless @action
+
+      @action
     end
 
     # sets target action for services
     # @param action[:start, :stop, :restart, :nothing] for possible values and
     #  its explanation please see return value of {target_action}
-    def target_action= (action)
+    def action= (action)
     end
 
     # returns currently set autostart configuration. If it is not yet set it
@@ -69,14 +93,33 @@ module Yast2
     #   - `:manual` all services and all its associated sockets is disabled.
     #   - `:inconsistent` mixture of states
     #   - `:unknown` when no state is set and read call failed.
-    def target_autostart
+    def autostart
+      read unless @autostart
+
+      @autostart
     end
 
     # sets autostart configuration.
     # @param [:on_boot, :on_demand, :manual, :inconsistent] autostart
     # configuratio. For explanation please see {target_autostart} when
     # `:inconsistent` means keep it as it is now.
-    def target_autostart=(configuration)
+    def autostart=(configuration)
+    end
+
+  private
+
+    def read_status
+      services_active = @services.map { |s| s.active? }
+
+      return @status = :active if services_active.all?
+      return @status = :inactive if services_active.none?
+      @status = :inconsistent
+    rescue Yast::SystemctlError => e
+      log.error "systemctl failure: #{e.inspect}"
+      @status = :unknown
+    end
+
+    def read_autostart
     end
   end
 end
