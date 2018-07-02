e152ac75d2f7a71b751ad35f4ec118dd0e035e84
Josef Reidinger <jreidinger@suse.cz>
2018-07-02 17:25:00 +0200

    add working examples

---
+++ library/systemd/examples/service_widget.rb
@@ -0,0 +1,46 @@
+require "yast"
+
+require "yast2/service_widget"
+require "yast2/service_configuration"
+
+Yast.import "SystemdService"
+
+def service
+  @service ||= Yast::SystemdService.find!("cups.service")
+end
+
+def service_configuration
+  @service_configuration ||= Yast2::ServiceConfiguration.new(service)
+end
+
+def service_widget
+  @service_widget ||= Yast2::ServiceWidget.new(service_configuration)
+end
+
+def read
+  service_configuration.read
+end
+
+include Yast::UIShortcuts
+
+def ui_loop
+  Yast::UI.OpenDialog(Yast::HBox(service_widget.content))
+  loop do
+    input = Yast::UI.UserInput
+    service_widget.handle_input(input)
+    break if input == :cancel
+  end
+  service_widget.store
+  Yast::UI.CloseDialog
+end
+
+def write
+  # intentionally nothing
+  true
+end
+
+read
+ui_loop
+write
+
+nil
