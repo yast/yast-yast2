d28258b706285bc400136042c6f9ecd8ad5c67f2
Josef Reidinger <jreidinger@suse.cz>
2018-07-25 12:52:48 +0200

    adapt Makefile and add cwm version of service widget

---
+++ library/cwm/examples/service_widget.rb
@@ -0,0 +1,80 @@
+# Simple example to demonstrate service widget API for CWM
+
+require_relative "example_helper"
+
+require "cwm"
+require "cwm/service_widget"
+require "yast2/system_service"
+
+Yast.import "CWM"
+Yast.import "Wizard"
+Yast.import "Popup"
+
+class LuckyNumberWidget < CWM::IntField
+  attr_reader :result, :minimum, :maximum
+
+  def initialize
+    @minimum = 0
+    @maximum = 1000
+  end
+
+  def label
+    _("Lucky number")
+  end
+
+  def store
+    @result = value
+  end
+end
+
+class GenerateButton < CWM::PushButton
+  def initialize(lucky_number_widget)
+    @lucky_number_widget = lucky_number_widget
+  end
+
+  def label
+    _("Generate Lucky Number")
+  end
+
+  def handle
+    Yast::Builtins.y2milestone("handle called")
+    @lucky_number_widget.value = rand(1000)
+
+    nil
+  end
+end
+
+module Yast
+  class ExampleDialog
+    include Yast::I18n
+    include Yast::UIShortcuts
+    def run
+      textdomain "example"
+
+      lucky_number_widget = LuckyNumberWidget.new
+      button_widget = GenerateButton.new(lucky_number_widget)
+      service = ::Yast2::SystemService.find("cups.service")
+      puts service.public_methods
+
+      contents = HBox(
+        button_widget,
+        lucky_number_widget,
+        ::CWM::ServiceWidget.new(service)
+      )
+
+      Yast::Wizard.CreateDialog
+      back_handler = proc { Yast::Popup.YesNo("Really go back?") }
+      abort_handler = proc { Yast::Popup.YesNo("Really abort?") }
+      CWM.show(contents,
+        caption:       _("Lucky number"),
+        back_handler:  back_handler,
+        abort_handler: abort_handler)
+      Yast::Wizard.CloseDialog
+
+      lucky_number_widget.result
+      # service.save # do not call to avoid real system modification
+    end
+  end
+end
+
+Yast::ExampleDialog.new.run
---
+++ library/cwm/src/lib/cwm/service_widget.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast2/service_widget"
+require "cwm/custom_widget"
+
+module CWM
+  class ServiceWidget < CustomWidget
+
+    # creates new widget instance for given service
+    # @param service_configuration [Yast2::SystemService,Yast2::CompoundService] service
+    def initialize(service)
+      @service_widget = Yast2::ServiceWidget.new(service)
+      self.handle_all_events = true
+    end
+
+    def contents
+      @service_widget.content
+    end
+
+    def handle(event)
+      @service_widget.handle_input(event["ID"])
+    end
+
+    def store
+      @service_widget.store
+    end
+  end
+end
+
