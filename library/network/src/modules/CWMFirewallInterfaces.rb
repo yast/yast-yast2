96b93570b59ab7eab7d4938d8ea7b1a394f1b98d
Imobach González Sosa <igonzalezsosa@suse.com>
2018-09-19 14:55:34 +0100

    Shorten variable name

--- library/network/src/modules/CWMFirewallInterfaces.rb
+++ library/network/src/modules/CWMFirewallInterfaces.rb
@@ -249,9 +249,9 @@ module Yast
       return if !configuration_changed
 
       zones =
-        known_interfaces.each_with_object([]) do |known_interface, memo|
-          if allowed_interfaces.include?(known_interface.name)
-            zone_name = known_interface.zone ? known_interface.zone.name : firewalld.default_zone
+        known_interfaces.each_with_object([]) do |iface, memo|
+          if allowed_interfaces.include?(iface.name)
+            zone_name = iface.zone ? iface.zone.name : firewalld.default_zone
             memo << zone_name
           end
         end
