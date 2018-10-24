e07a34acb7e44e1f428435763af902247cf15353
Josef Reidinger <jreidinger@suse.cz>
2018-10-24 17:53:09 +0200

    Use LSB compliment path for temporary mount and ensure it exists

---
+++ library/control/test/installation_test.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/env rspec
+
+require_relative "test_helper"
+
+Yast.import "Installation"
+
+describe Yast::Installation do
+  subject { Yast::Installation }
+
+  describe ".sourcedir" do
+    before do
+      allow(::File).to receive(:exist?).and_return(true)
+    end
+
+    it "returns string" do
+      expect(subject.sourcedir).to eq "/run/YaST2/mount"
+    end
+
+    it "ensures that directory exists" do
+      expect(::File).to receive(:exist?).and_return(false)
+      expect(::FileUtils).to receive(:mkdir_p)
+
+      subject.sourcedir
+    end
+  end
+end
+
+
