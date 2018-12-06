b14de03b1c54d9a45e204b25b4abbc1adbce3097
Stefan Schubert <schubi@suse.de>
2018-12-06 10:44:05 +0100

    adapted suggestions

--- library/systemd/src/lib/yast2/system_service.rb
+++ library/systemd/src/lib/yast2/system_service.rb
@@ -154,7 +154,7 @@ module Yast2
       #
       # @param names [Array<String>] service names to find
       #
-      # @return [Array<SystemService>] not found: SystemService.service is nil
+      # @return [Array<SystemService>] when not found then the SystemService.servie is nil for the respective service in the list
       def find_many(names)
         Yast::SystemdService.find_many(names).map { |s| new(s) }
       end
