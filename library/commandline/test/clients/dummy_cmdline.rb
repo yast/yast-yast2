e432f30d1ed0eb3881652de690efd6a25d5d82fa
Martin Vidner <mvidner@suse.cz>
2019-08-08 15:48:09 +0200

    tests should use the "example" textdomain
    Co-Authored-By: Ladislav Slezák <lslezak@suse.cz>

--- library/commandline/test/clients/dummy_cmdline.rb
+++ library/commandline/test/clients/dummy_cmdline.rb
@@ -2,7 +2,7 @@ module Yast
   class DummyCmdlineClient < Client
     def main
       Yast.import "CommandLine"
-      textdomain "base"
+      textdomain "example"
 
       # Command line definition
       cmdline = {
