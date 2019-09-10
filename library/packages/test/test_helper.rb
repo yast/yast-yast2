6eaf34e7f4b052cf411710c101803abe05dbe428
Ladislav Slezák <lslezak@suse.cz>
2019-09-10 17:39:05 +0200

    Fixed build
    We need to mock the Yast::InstURL module completely to
    avoid circular yast2 <-> yast2-packager build dependency.

--- library/packages/test/test_helper.rb
+++ library/packages/test/test_helper.rb
@@ -2,3 +2,15 @@ require_relative "../../../test/test_helper.rb"
 require "pathname"
 
 PACKAGES_FIXTURES_PATH = Pathname.new(File.dirname(__FILE__)).join("data")
+
+# mock missing YaST modules
+module Yast
+  # we cannot depend on this module (circular dependency)
+  class InstURLClass
+    def installInf2Url(_extra_dir = "")
+      ""
+    end
+  end
+
+  InstURL = InstURLClass.new
+end
