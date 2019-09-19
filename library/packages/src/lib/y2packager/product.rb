f03ddb089cdc33ef93d79ad533d347ae55d83b34
Ladislav Slezák <lslezak@suse.cz>
2019-09-19 12:41:17 +0200

    Added ProductControlProduct

--- library/packages/src/lib/y2packager/product.rb
+++ library/packages/src/lib/y2packager/product.rb
@@ -13,7 +13,7 @@
 Yast.import "Pkg"
 require "y2packager/product_reader"
 require "y2packager/release_notes_reader"
-require "y2packager/product_license"
+require "y2packager/product_license_mixin"
 
 module Y2Packager
   # Represent a product which is present in a repository. At this
@@ -22,6 +22,7 @@ module Y2Packager
   # repositories).
   class Product
     include Yast::Logger
+    include ProductLicenseMixin
 
     # @return [String] Name
     attr_reader :name
@@ -221,69 +222,6 @@ module Y2Packager
     # @return [String] Package label
     def label
       display_name || short_name || name
-    end
-
-    # Return the license to confirm
-    #
-    # @param lang [String] Language
-    # @return [ProductLicense,nil] Product's license; nil if the license was not found.
-    def license
-      @license ||= ProductLicense.find(name)
-    end
-
-    # Return the license text to be confirmed
-    #
-    # @param lang [String] Language
-    # @return [String] Product's license; empty string ("") if no license was found.
-    def license_content(lang)
-      return "" unless license?
-
-      license.content_for(lang)
-    end
-
-    # Determines whether the product has a license
-    #
-    # @param lang [String] Language
-    # @return [Boolean] true if the product has a license
-    def license?
-      !!license
-    end
-
-    # Determine whether the license should be accepted or not
-    #
-    # @return [Boolean] true if the license acceptance is required
-    def license_confirmation_required?
-      return false unless license?
-
-      license.confirmation_required?
-    end
-
-    # Set license confirmation for the product
-    #
-    # @param confirmed [Boolean] determines whether the license should be accepted or not
-    def license_confirmation=(confirmed)
-      return unless license
-
-      confirmed ? license.accept! : license.reject!
-    end
-
-    # Determine whether the license is confirmed
-    #
-    # @return [Boolean] true if the license was confirmed (or acceptance was not needed)
-    def license_confirmed?
-      return false unless license
-
-      license.accepted? || !license_confirmation_required?
-    end
-
-    # [String] Default license language.
-    DEFAULT_LICENSE_LANG = "en_US".freeze
-
-    # Return available locales for product's license
-    #
-    # @return [Array<String>] Language codes ("de_DE", "en_US", etc.)
-    def license_locales
-      license.locales
     end
 
     # Return product's release notes
---
+++ library/packages/src/lib/y2packager/product_control_product.rb
@@ -0,0 +1,102 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2019 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE.
+#
+# To contact SUSE about this file by physical or electronic mail, you may find
+# current contact information at www.suse.com.
+# ------------------------------------------------------------------------------
+
+require "yast"
+require "y2packager/product_license_mixin"
+
+Yast.import "Arch"
+Yast.import "Linuxrc"
+Yast.import "ProductFeatures"
+
+module Y2Packager
+  # This class implements a base product read from the control.xml file.
+  class ProductControlProduct
+    # initialize the selected base product
+    @selected = nil
+
+    class << self
+      attr_accessor :selected
+    end
+
+    extend Yast::Logger
+    include ProductLicenseMixin
+
+    attr_reader :name, :version, :arch, :label, :license_url, :register_target
+
+    #
+    # Read the base products from the control.xml file. The products for the incompatible
+    # machine architecture and the hidden products are filtered out.
+    #
+    # @return [Array<Installation::ProductControlProduct>] List of the products
+    def self.products
+      return @products if @products
+
+      control_products = Yast::ProductFeatures.GetFeature("software", "base_products")
+      arch = Yast::Arch.architecture
+      linuxrc_products = (Yast::Linuxrc.InstallInf("specialproduct") || "").split(",").map(&:strip)
+
+      @products = control_products.each_with_object([]) do |p, array|
+        # a hidden product requested?
+        if p["special_product"] && !linuxrc_products.include?(p["name"])
+          log.info "Skipping special hidden product #{p["name"]}"
+          next
+        end
+
+        # compatible arch?
+        if p["archs"] && !p["archs"].split(",").map(&:strip).include?(arch)
+          log.info "Skipping product #{p["name"]} - not compatible with arch #{arch}"
+          next
+        end
+
+        array << new(
+          name:            p["name"],
+          version:         p["version"],
+          arch:            arch,
+          label:           p["label"],
+          license_url:     p["license_url"],
+          # expand the "$arch" placeholder
+          register_target: (p["register_target"] || "").gsub("$arch", arch)
+        )
+      end
+    end
+
+    # Constructor
+    # @param name [String] product name (the identifier, e.g. "SLES")
+    # @param version [String] version ("15.2")
+    # @param arch [String] The architecture ("x86_64")
+    # @param label [String] The user visible name ("SUSE Linux Enterprise Server 15 SP2")
+    # @param license_url [String] License URL
+    # @param register_target [String] The registration target name used
+    #   for registering the product, the $arch variable is replaced
+    #   by the current machine architecture
+    def initialize(name:, version:, arch:, label:, license_url:, register_target:)
+      @name = name
+      @version = version
+      @arch = arch
+      @label = label
+      @license_url = license_url
+      @register_target = register_target
+    end
+
+    # Is the product selected?
+    # @return [Boolean] true if the product is the selected base product
+    def selected?
+      self == self.class.selected
+    end
+  end
+end
