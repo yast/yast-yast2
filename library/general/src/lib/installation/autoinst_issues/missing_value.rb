f8f854a4607a488463980bd60a0ebfb5a0ae4276
Stefan Schubert <schubi@suse.de>
2020-05-28 11:29:38 +0200

    copy rights

--- library/general/src/lib/installation/autoinst_issues/missing_value.rb
+++ library/general/src/lib/installation/autoinst_issues/missing_value.rb
@@ -1,4 +1,4 @@
-# Copyright (c) [2018] SUSE LLC
+# Copyright (c) [2020] SUSE LLC
 #
 # All Rights Reserved.
 #
--- library/general/test/autoinst_issues/invalid_value_test.rb
+++ library/general/test/autoinst_issues/invalid_value_test.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env rspec
-# Copyright (c) [2018] SUSE LLC
+# Copyright (c) [2020] SUSE LLC
 #
 # All Rights Reserved.
 #
