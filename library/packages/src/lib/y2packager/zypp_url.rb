5aed8408b76949e65aecbeddb7c7d0e01d6e1919
Ancor Gonzalez Sosa <ancor@suse.de>
2020-07-14 16:15:15 +0200

    Fix wrong copyright header

--- library/packages/src/lib/y2packager/zypp_url.rb
+++ library/packages/src/lib/y2packager/zypp_url.rb
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2017-2020 SUSE LLC, All Rights Reserved.
+# Copyright (c) 2020 SUSE LLC, All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of version 2 of the GNU General Public License as published by the
