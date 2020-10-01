73525e0c4b8cc321e8cefb6b12e6900bd121e22c
Knut Alejandro Anderssen González <teclator@gmail.com>
2020-10-01 16:16:02 +0100

    Revert "Drop SuSEfirewall2 completely"
    This reverts commit 4e4ae713ab36ef7c164f75077c04f650a4c0a8ff.

---
+++ library/network/src/modules/SuSEFirewallServices.rb
@@ -0,0 +1,34 @@
+# ***************************************************************************
+#
+# Copyright (c) 2002 - 2016 Novell, Inc.
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail,
+# you may find current contact information at www.novell.com
+#
+# ***************************************************************************
+#
+# File:  modules/SuSEFirewallServices.rb
+# Package:  Firewall Services, Ports Aliases.
+# Summary:  Definition of Supported Firewall Services and Port Aliases.
+# Authors:  Markos Chandras <mchandras@suse.de>
+#
+# Global Definition of Firewall Services
+# Manages services for SuSEFirewall2 and FirewallD
+
+require "yast"
+require "network/susefirewalldservices"
+
+Yast::SuSEFirewallServices = Yast::SuSEFirewalldServicesClass.new
