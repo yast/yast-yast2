54fb57cb742a01267ed00c91f25fdf9b618d7ec7
Ladislav Slezák <lslezak@suse.cz>
2020-11-27 13:47:55 +0100

    Move to GitHub actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -40,12 +40,20 @@ if ENV["COVERAGE"]
   # track all ruby files under src
   SimpleCov.track_files("#{top_location}/**/src/**/*.rb")
 
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
+  # additionally use the LCOV format for on-line code coverage reporting at CI
+  if ENV["CI"] || ENV["COVERAGE_LCOV"]
+    require "simplecov-lcov"
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      # this is the default Coveralls GitHub Action location
+      # https://github.com/marketplace/actions/coveralls-github-action
+      c.single_report_path = "coverage/lcov.info"
+    end
+
     SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
       SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
+      SimpleCov::Formatter::LcovFormatter
     ]
   end
 end
