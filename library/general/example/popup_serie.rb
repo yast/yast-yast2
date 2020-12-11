4843d62af0057c1cf24d728f467d3fa63111f4a8
Josef Reidinger <jreidinger@suse.cz>
2020-12-11 16:56:39 +0100

    add example of also long details

--- library/general/example/popup_serie.rb
+++ library/general/example/popup_serie.rb
@@ -6,6 +6,8 @@ Yast2::Popup.show("Simple text")
 
 Yast2::Popup.show("First paragraph.\n\n" + "Long text without newlines. " * 50)
 
+Yast2::Popup.show("First paragraph.", details: "Long text without newlines. " * 50)
+
 Yast2::Popup.show("Long text\n" * 50)
 
 Yast2::Popup.show("Simple text with details", details: "More details here")
