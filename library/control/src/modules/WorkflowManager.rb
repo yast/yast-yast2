a6d063726ef80f6b875b20cf1bb9d6e0c470326d
Ladislav Slezák <lslezak@suse.cz>
2021-01-13 17:21:59 +0100

    Keep the libzypp target open to verify other packages (bsc#1180858)
    - Related to the previous fix (bsc#1179773)
    - 4.2.90

--- library/control/src/modules/WorkflowManager.rb
+++ library/control/src/modules/WorkflowManager.rb
@@ -1681,7 +1681,6 @@ module Yast
         # libzypp needs the target for verifying the GPG signatures of the downloaded packages
         Pkg.TargetInitialize("/") if Stage.initial
         downloader.download(tmp.path)
-        Pkg.TargetFinish() if Stage.initial
 
         extract(tmp.path, dir)
         # the RPM package file is not needed after extracting it's content,
