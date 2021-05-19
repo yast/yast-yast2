ff8b8e0b33609c3d689601419ac469a87617d44f
Ladislav Slezák <lslezak@suse.cz>
2021-05-19 18:37:10 +0200

    Dump more data

--- library/general/src/lib/installation/installation_data.rb
+++ library/general/src/lib/installation/installation_data.rb
@@ -21,9 +21,9 @@ require "installation/installation_info"
 require "y2packager/medium_type"
 require "y2packager/product_reader"
 require "y2packager/resolvable"
-require "y2packager/repository"
 
 Yast.import "Mode"
+Yast.import "Pkg"
 
 module Installation
   #
@@ -41,15 +41,11 @@ module Installation
           # media detection and downloading repository metadata if the value is not
           # cached yet. Fetch the internal cache value directly, not nice but safe...
           "medium_type"             => Y2Packager::MediumType.instance_variable_get("@type"),
-          "repositories"            => Y2Packager::Repository.all.map do |repo|
-            {
-              "id"      => repo.repo_id,
-              "alias"   => repo.repo_alias,
-              "name"    => repo.name,
-              "url"     => repo.url.to_s,
-              "dir"     => repo.product_dir,
-              "enabled" => repo.enabled?
-            }
+          "repositories"            => Yast::Pkg.SourceGetCurrent(false).map do |repo|
+            Yast::Pkg.SourceGeneralData(repo)
+          end,
+          "services"                => Yast::Pkg.ServiceAliases.map do |s|
+            Yast::Pkg.ServiceGet(s)
           end,
           "available_base_products" => Y2Packager::ProductReader.new.available_base_products.map do |product|
             {
@@ -65,7 +61,9 @@ module Installation
               "version"      => product.version,
               "display_name" => product.display_name,
               "status"       => product.status,
-              "vendor"       => product.vendor
+              "vendor"       => product.vendor,
+              "repository"   => product.source,
+              "path"         => product.path
             }
           end
         }
