9490cc749734e0a2e4dca619c5410b77a9d44500
Imobach González Sosa <igonzalezsosa@suse.com>
2021-06-08 10:19:43 +0100

    Ignore no *.conf files in sysctl directories

--- library/general/src/lib/cfa/sysctl_config.rb
+++ library/general/src/lib/cfa/sysctl_config.rb
@@ -157,7 +157,9 @@ module CFA
       if Yast::FileUtils.IsFile(path)
         [path]
       elsif Yast::FileUtils.IsDirectory(path)
-        Yast::SCR.Read(Yast::Path.new(".target.dir"), path).map { |file| File.join(path, file) }
+        Yast::SCR.Read(Yast::Path.new(".target.dir"), path)
+          .select { |f| f.end_with?(".conf") } # according to 'sysctl.conf' manpage, only .conf files are considered
+          .map { |f| File.join(path, f) }
       else
         log.debug("Ignoring not valid path: #{path}")
 
