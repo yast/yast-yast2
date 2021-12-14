3a4da28d574715115203a3a2c9cbe099bc55ed6f
Imobach González Sosa <igonzalezsosa@suse.com>
2021-12-14 23:06:46 +0000

    Do not check WFM internals in the commandline test

--- library/commandline/test/commandline_test.rb
+++ library/commandline/test/commandline_test.rb
@@ -25,7 +25,7 @@ describe Yast::CommandLine do
 
   it "displays errors and aborts" do
     expect(STDOUT).to receive(:puts).with("Initialize called").ordered
-    expect(Yast::CommandLine).to receive(:Print).with(/I crashed/).ordered
+    expect(Yast::WFM).to receive(:handle_exception).with(RuntimeError, /dummy_cmdline/).ordered
     expect(STDOUT).to_not receive(:puts).with("Finish called")
 
     Yast::WFM.CallFunction("dummy_cmdline", ["crash"])
