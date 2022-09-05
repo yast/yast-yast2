faac996b4659893782cf226d56fb6d5e09f25234
Knut Anderssen <kanderssen@suse.com>
2022-09-05 08:27:39 +0100

    Try to make documentation more clear

--- library/network/src/modules/NetworkService.rb
+++ library/network/src/modules/NetworkService.rb
@@ -237,8 +237,14 @@ module Yast
       @cached_name = nil
     end
 
-    # Helper to apply a change of the network service
-    # @param force [Boolean] whether the service should forced to be enabled or not
+    # Helper to apply a change of the network service enabling the service selected and taking care
+    # of disabling the current one in case of modified.
+    #
+    # TODO: this method could take care of disabling all the services except the one selected in
+    #   order to prevent that multiple backends are enabled at the same time.
+    #
+    # @param force [Boolean] whether the service should forced to be enabled or not; it will not
+    #   disable other services apart of the one currently in use in case of modified
     def EnableDisableNow(force: false)
       return unless force || Modified()
 
