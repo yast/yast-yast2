1a636a31cbc9b7b322693a84e8b4033d7e2df25c
Stefan Schubert <schubi@suse.de>
2025-03-12 11:36:59 +0100

    added testcase

--- library/general/src/modules/Arch.rb
+++ library/general/src/modules/Arch.rb
@@ -433,7 +433,7 @@ module Yast
     #
     # @return [Boolean] true if a TPM2 chip is available; false otherwise
     def has_tpm2
-      @_has_tpm2 = SCR.Read(path(".target.string"), "/sys/module/tpm/version").strip == "2.0" if @_has_tpm2.nil?
+      @_has_tpm2 = SCR.Read(path(".target.string"), "/sys/module/tpm/version")&.strip == "2.0" if @_has_tpm2.nil?
       @_has_tpm2
     end
 
